559dd4e9ca6b795538a8be3854190e21e9fbc926
SuyashBhavalkar3 <suyashbhavalkar82@gmail.com>
2026-05-02 00:14:41 +0530

    fix: use relative path for swagger server url

--- backend/src/app.js
+++ backend/src/app.js
@@ -45,7 +45,8 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 5000}`,
+        url: '/',
+        description: 'Default Server',
       },
     ],
     components: {
